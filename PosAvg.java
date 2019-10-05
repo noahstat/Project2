9378525e05c8574f9251e5c2bfa3a81a41309d4b
Noah Stasyszen <stasyszen@gmail.com>
2019-10-05 14:04:53 -0500

    implemented the method hopefully correctly, now have a toString and getAverages

--- PosAvg.java
+++ PosAvg.java
@@ -8,16 +8,54 @@ generate error. Therefore, you can skip giving the first two stations or last tw
 public class PosAvg extends MesoInherit
 {
 	
-	private int index = 0;
+	private static int index = 0;
 	
 	public PosAvg(String ID)
 	{	
-			super(ID);
+		super(ID);
 			
 		//find the index of the element for other calculations
 		this.index = Arrays.binarySearch(MesoInherit.stations, 0, MesoInherit.size, ID);
 		
 	}
 	
+	public int indexOfStation()
+	{
+		return index;
+	}
+	
+	@Override
+	public String toString()
+	{
+		//output should look like:
+		//This index is average of NOWA and OILT, NEWP and OKCE, and so on.
+		
+		String[] out = getAverages();
+		
+		String string = "This index is average of " + out[0] + " and " + out[1] + ", " 
+				+ out[2] + " and " + out[3] + ", and so on.";
+		
+		return string;
+	}
+
+	public String[] getAverages() 
+	{
+		//first two will be one average and second two will be the other averages
+		String[] aves = new String[4];
+		
+		if(index < 2 || index > (size - 2))
+		{
+			return null;
+		}
+		else
+		{
+			//i is where we are getting the info and k is where we're storing the info both are there for simplicity
+			for(int i = index - 2, k = 0; i < index + 4;++i, ++k)
+			{
+				aves[k] = stations[i];
+			}
+			return aves;
+		}
+	}
 	
 }
