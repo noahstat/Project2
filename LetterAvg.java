86ef2d7d3c22d6e1dadfddcd420c72f93cac2eba
Noah Stasyszen <stasyszen@gmail.com>
2019-10-06 21:11:21 -0500

    added a letter average method

--- LetterAvg.java
+++ LetterAvg.java
@@ -6,5 +6,40 @@ public class LetterAvg extends MesoInherit
 	{
 		super(stId);
 	}
+	
+	public char letterAverage()
+	{
+		//char.getNumericValue(char c);
+		String stID = getStID();
+		int ceil = 0;
+		int floor = 0;
+		int round = 0;
+		double sum = 0.0;
+		
+		for(int i = 0; i < 4; ++i)
+		{
+			char c = stID.charAt(i);
+			sum += Character.getNumericValue(c);
+			
+		}
+		
+		/*Ascii Ceiling is 79
+		Ascii Floor is 78
+		Ascii Average is 79*/
+		
+		ceil = (int)Math.ceil(sum);
+		System.out.println("Ascii Ceiling is " + ceil);
+		floor = (int)Math.floor(sum);
+		System.out.println("Ascii Floor is " + floor);
+		round = (int)Math.round(sum);
+		System.out.println("Ascii Average is " + round);
+		
+		
+		/*You have to get the ASCII value for N R M and N as 78, 82, 77, and 78. Sum of these ASCII value is 315. Dividing
+		315 by 4, we get 78.75*/
+		
+		return 'O';
+		
+	}
 
 }
