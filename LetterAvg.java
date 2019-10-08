410bba759d2ab90552b55c103657e440650ea51a
Noah Stasyszen <stasyszen@gmail.com>
2019-10-07 22:47:16 -0500

    added some more methods

--- LetterAvg.java
+++ LetterAvg.java
@@ -6,8 +6,35 @@ public class LetterAvg extends MesoInherit
 	{
 		super(stId);
 	}
-	
+
 	public char letterAverage()
+	{
+		//char.getNumericValue(char c);
+		String stID = getStID();
+		int ceil = 0;
+		int floor = 0;
+		int round = 0;
+		double sum = 0.0;
+		
+		for(int i = 0; i < 4; ++i)
+		{
+			char c = stID.charAt(i);
+			sum += Character.getNumericValue(c);
+			
+		}
+		
+		ceil = (int)Math.ceil(sum);
+//		System.out.println("Ascii Ceiling is " + ceil);
+		floor = (int)Math.floor(sum);
+//		System.out.println("Ascii Floor is " + floor);
+		round = (int)Math.round(sum);
+//		System.out.println("Ascii Average is " + round);
+		
+		return 'O';
+		
+	}
+	
+	public int[] calAverage()
 	{
 		//char.getNumericValue(char c);
 		String stID = getStID();
@@ -28,17 +55,17 @@ public class LetterAvg extends MesoInherit
 		Ascii Average is 79*/
 		
 		ceil = (int)Math.ceil(sum);
-		System.out.println("Ascii Ceiling is " + ceil);
+//		System.out.println("Ascii Ceiling is " + ceil);
 		floor = (int)Math.floor(sum);
-		System.out.println("Ascii Floor is " + floor);
+//		System.out.println("Ascii Floor is " + floor);
 		round = (int)Math.round(sum);
-		System.out.println("Ascii Average is " + round);
-		
+//		System.out.println("Ascii Average is " + round);
+		int[] calculated = {ceil, floor, round};
 		
 		/*You have to get the ASCII value for N R M and N as 78, 82, 77, and 78. Sum of these ASCII value is 315. Dividing
 		315 by 4, we get 78.75*/
 		
-		return 'O';
+		return calculated;
 		
 	}
 
