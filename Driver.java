e7d884000ba47fee4f65ccd12de37e15ac58a592
Noah Stasyszen <stasyszen@gmail.com>
2019-10-09 10:22:41 -0500

    changed STID so i can test some more stuff

--- Driver.java
+++ Driver.java
@@ -8,7 +8,7 @@ public class Driver
 {
 	public static void main(String[] args) throws IOException 
 	{
-		String stID = "NRMN";
+		String stID = "IIII";
 		MesoInherit mesoInherit = new MesoInherit(new MesoStation(stID));
 		
 		PosAvg posAvg = new PosAvg(stID);
